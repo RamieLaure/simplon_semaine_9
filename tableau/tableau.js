5a449c5ebcf578dcb032045b717a2e6b22699804
RamieLaure <marielaure.leneannec@gmail.com>
2015-11-24 22:50:00 +0000

    maj

--- tableau/tableau.js
+++ tableau/tableau.js
@@ -1,30 +1,55 @@
-$liste = document.getElementById("liste");
-$texte = document.getElementById("texte");
+$list = document.getElementById("list");
+$prenom = document.getElementById("prenom");
+$nom = document.getElementById("nom");
+$tel = document.getElementById("tel");
 $ajouter = document.getElementById("ajouter");
 $annuler = document.getElementById("annuler");
+$research = document.getElementById("research");
+$search = document.getElementById("search");
+$result = document.getElementById("result");
 
+contacts = [{
+    prenom: "Julien",
+    nom: "Grillot",
+    tel: "0123456789"
+}, {
+    prenom: "John",
+    nom: "Smith",
+    tel: "0123456789"
+}];
 
-tableau = ["Bob", "Julien", "Roger"];
+function texteContact(contact) {
+    return contact.prenom + " " + contact.nom + " (" + contact.tel + ")";
+}
 
-function afficherListe() {
+function showList() {
     elements = "";
-    for (i = 0; i < tableau.length; i++) {
-        elements += "<li>" + tableau[i] + "</li>";
+    for (i = 0; i < contacts.length; i++) {
+        elements += "<li>" + texteContact(contacts[i]) + "</li>";
     }
-    $liste.innerHTML = elements;
+    $list.innerHTML = elements;
 }
 
 function ajouter() {
-    tableau.push($texte.value);
-    afficherListe();
+    contacts.push({
+        prenom: $prenom.value,
+        nom: $nom.value,
+        tel: $tel.value
+    });
+    showList();
 }
 
-function annuler() {
-    tableau.pop();
-    afficherListe();
+function search() {
+    research = $research.value;
+    elements = "";
+    for (i = 0; i < contacts.length; i++) {
+        if (contacts[i].prenom == research || contacts[i].nom == research || contacts[i].tel == research) {
+            elements += "<li>" + texteContact(contacts[i]) + "</li>";
+        }
+    }
+    $result.innerHTML = elements;
 }
-
 
 $ajouter.onclick = ajouter;
-$annuler.onclick = annuler;
-afficherListe();
+$search.onclick = search;
+showList();
